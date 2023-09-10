aff5a057d5698a7ee6402251d762f243733ea3ce
mikshiba <mik_shiba@icloud.com>
2023-09-10 23:46:25 +0900

    ch6まで完成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobarrr", password_digest: "foobarrr")
   end
 
   test "バリデーションの有効性テスト" do
@@ -32,11 +32,43 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "様々なメールアドレスでテストする" do
+  test "様々な有効なメールアドレスでテストする" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} should be valid"
+      assert @user.valid?, "#{valid_address.inspect}、バリデーションをかけてください"
     end
   end
+
+  test "様々な無効なメールアドレスでテストする" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect}、無効なアドレスです。バリデーションをかけてください"
+    end
+  end
+
+  test "メールアドレスが重複していないかのテスト" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "メールアドレスを小文字にして保存できているかのテスト" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "パスワードが空ではいけないテスト" do
+    @user.password = @user.password_confirmation = " " * 8
+    assert_not @user.valid?
+  end
+
+  test "パスワードが8文字以上であるテスト" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?
+  end
+
 end
