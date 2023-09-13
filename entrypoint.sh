84e3303687de9d95e2c67b27093c9b019f298f35
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-14 01:10:30 +0900

    ch7.1.2まで完了

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
+bundle exec rails db:migrate
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
