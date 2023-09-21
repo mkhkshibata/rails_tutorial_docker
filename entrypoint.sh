6581bcddc8e2b94f4229f72636f5a2defbb6c7e4
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-21 22:58:20 +0900

    entrypoint修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
-bundle exec rails db:migrate:reset
+bundle exec rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:seed
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
