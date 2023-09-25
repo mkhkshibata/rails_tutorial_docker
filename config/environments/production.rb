85a38d55df08bddb28970415cdc887f052104e65
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-25 19:22:52 +0900

    text修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_method = :smtp
   host = "rails-tutorial-docker-dopu.onrender.com"
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_setting = {
+  ActionMailer::Base.smtp_settings = {
     :port => 587,
     :address => 'smtp.mailgun.org',
     :user_name => ENV['MAILGUN_SMTP_LOGIN'],
     :password => ENV['MAILGUN_SMTP_PASSWORD'],
-    # :domain => host,
-    # :authentication => plain
+    :domain => host,
+    :authentication => plain
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
