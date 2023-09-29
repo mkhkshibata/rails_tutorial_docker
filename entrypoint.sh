508bd69a8c28249d9db67e9bae73e51b41e6dc13
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-29 23:01:17 +0900

    seedなし

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
 bundle exec rails assets:clean
 bundle exec rails assets:precompile
-bundle exec rails db:seed
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
