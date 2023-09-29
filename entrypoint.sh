f2dfa048708291924ac529f23347028aae605585
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-29 23:22:36 +0900

    rakeに変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
-bundle exec rails assets:clean
-bundle exec rails assets:precompile
+bundle exec rake assets:clean
+bundle exec rake assets:precompile
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
