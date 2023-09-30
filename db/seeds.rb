8d23142b1931366b1ec8682c42252f1b4ec8f39c
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-30 13:47:15 +0900

    seedコメントアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: "サンプルユーザー",
-						email: "example@rails.com",
-						password: "foobarrr",
-						password_confirmation: "foobarrr",
-						admin: true,
-						activated: true,
-						activated_at: Time.zone.now)
+# User.create(name: "サンプルユーザー",
+# 						email: "example@rails.com",
+# 						password: "foobarrr",
+# 						password_confirmation: "foobarrr",
+# 						admin: true,
+# 						activated: true,
+# 						activated_at: Time.zone.now)
 
-99.times do |n|
-	Faker::Config.locale = :ja
-	name = Faker::Name.name
-	email = "example-#{n + 1}@railstutorial.org"
-	password = "password"
-	User.create!(name: name,
-								email: email,
-								password: password,
-								password_confirmation: password,
-								activated: true,
-								activated_at: Time.zone.now)
-end
+# 99.times do |n|
+# 	Faker::Config.locale = :ja
+# 	name = Faker::Name.name
+# 	email = "example-#{n + 1}@railstutorial.org"
+# 	password = "password"
+# 	User.create!(name: name,
+# 								email: email,
+# 								password: password,
+# 								password_confirmation: password,
+# 								activated: true,
+# 								activated_at: Time.zone.now)
+# end
 
-User.create(name: "test05",
-	email: "test05@test.jp",
-	password: "asdfasdf",
-	password_confirmation: "asdfasdf",
-	admin: true,
-	activated: true,
-	activated_at: Time.zone.now)
+# User.create(name: "test05",
+# 	email: "test05@test.jp",
+# 	password: "asdfasdf",
+# 	password_confirmation: "asdfasdf",
+# 	admin: true,
+# 	activated: true,
+# 	activated_at: Time.zone.now)
