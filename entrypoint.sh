1bb80bbe23b123e12e7f5db2587a3cec46b0e8ef
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-30 16:09:08 +0900

    env追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
 bundle exec rails assets:clean
 bundle exec rails assets:precompile
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset && bundle exec rake db:seed
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:seed RAILS_ENV=production
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
