fe29041493ec46b87abf98581c31ab809902de07
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-30 23:46:22 +0900

    2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
-bundle exec rails assets:clean
-bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails assets:precompile
 # RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 # bundle exec rails db:create RAILS_ENV=production
 # bundle exec rails db:migrate RAILS_ENV=production
