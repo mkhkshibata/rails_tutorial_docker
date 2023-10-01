af58aa11c58791e1ebc49b4fb4f74645054e6dbb
mkhkshibata <mkhk.shibata@gmail.com>
2023-10-02 00:55:29 +0900

    db

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 rm -f /rails_tutorial_docker/tmp/pids/server.pid
 bundle exec rails assets:clean
 bundle exec rails assets:precompile
-# RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-# bundle exec rails db:seed RAILS_ENV=production
+RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
+bundle exec rails db:seed RAILS_ENV=production
 # RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 # bundle exec rails db:create RAILS_ENV=production
 # bundle exec rails db:migrate RAILS_ENV=production
