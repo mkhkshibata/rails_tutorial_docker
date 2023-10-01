d79194bc64d05ffe3b93e41d186c236b0fbd3211
mkhkshibata <mkhk.shibata@gmail.com>
2023-10-01 12:04:19 +0900

    1s

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,8 @@ class AccountActivationsController < ApplicationController
 	#有効化用リンクでアクセスした時、パスワードを介さずDBを更新する
 	def edit
 		user = User.find_by(email: params[:email])
-		if user && !user.activated? && user.authenticated?(:activation, params[:id])
+		# if user && !user.activated? && user.authenticated?(:activation, params[:id])
+		if user && !user.activated?
 			user.activate
 			log_in(user)
 			flash[:success] = "アカウントが有効になりました"
