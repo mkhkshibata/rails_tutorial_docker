be9e4f3af82ed3b86e32ec8eacceefa7731240a2
mkhkshibata <mkhk.shibata@gmail.com>
2023-10-01 19:53:57 +0900

    komentout

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,14 +4,14 @@ class AccountActivationsController < ApplicationController
 	def edit
 		user = User.find_by(email: params[:email])
 		# if user && !user.activated? && user.authenticated?(:activation, params[:id])
-		if !user.activated? && user.authenticated?(:activation, params[:id])
+		if user && !user.activated? && user.authenticated?(:activation, params[:id])
 			user.activate
 			log_in(user)
 			flash[:success] = "アカウントが有効になりました"
 			redirect_to user
-		else
-			flash[:danger] = "無効なリンクです"
-			redirect_to root_url
-		end
+		# else
+		# 	flash[:danger] = "無効なリンクです"
+		# 	redirect_to root_url
+		# end
 	end
 end
