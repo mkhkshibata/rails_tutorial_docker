4c36fac7b21759d712b3dab54639af3351e76477
mkhkshibata <mkhk.shibata@gmail.com>
2023-10-01 20:30:15 +0900

    root

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -8,7 +8,7 @@ class AccountActivationsController < ApplicationController
 			user.activate
 			log_in(user)
 			flash[:success] = "アカウントが有効になりました"
-			redirect_to user_url(user)
+			redirect_to root_url
 		# else
 		# 	flash[:danger] = "無効なリンクです"
 		# 	redirect_to root_url
