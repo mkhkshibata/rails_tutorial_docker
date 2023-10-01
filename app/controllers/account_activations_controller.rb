87bec2894a4c14aa3428202419ddd8cc6af512ce
mkhkshibata <mkhk.shibata@gmail.com>
2023-10-01 13:06:41 +0900

    userのみ

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,7 +4,7 @@ class AccountActivationsController < ApplicationController
 	def edit
 		user = User.find_by(email: params[:email])
 		# if user && !user.activated? && user.authenticated?(:activation, params[:id])
-		if user && !user.activated? && user.authenticated?(:activation, params[:id])
+		if user
 			user.activate
 			log_in(user)
 			flash[:success] = "アカウントが有効になりました"
